8ef3417b1cebfb59826b31c39844d5ece1532eab
Bryan Albert <bryanalbert@hotmail.com>
2025-05-12 10:55:40 -0700

    Move logos to a footer.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import  Footer from './Footer';
 import './styles/App.css';
 
 const App = () => {
@@ -8,26 +7,13 @@ const App = () => {
 
 	return (
 		<>
-			<div>
-				<a href="https://vite.dev" target="_blank" rel="noreferrer noopener">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank" rel="noreferrer noopener">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React + Love</h1>
+			<h1>Conway's Game of Life</h1>
 			<div className="card">
 				<button type='button' onClick={() => setCount(count => count + 1)}>
 					count is {count}
 				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
 			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
+		<Footer />
 		</>
 	)
 }
---
+++ src/Footer.tsx
@@ -0,0 +1,20 @@
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './styles/Footer.css';
+
+const Footer = () => {
+  return (
+		<div className="footer">
+      <p>Built with Vite and React
+        <a href="https://vite.dev" target="_blank" rel="noreferrer noopener">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer noopener">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </p>
+    </div>
+  )
+}
+
+export default Footer;
