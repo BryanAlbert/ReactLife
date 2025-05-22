830a3b05eb6453844bbf0562cbfadd9d9836e668
Bryan Albert <bryanalbert@hotmail.com>
2025-05-22 13:20:25 -0700

    Add dark theme to About dialog.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,4 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material";
+import { Button, createTheme, Dialog, DialogActions, DialogContent, DialogTitle, ThemeProvider } from "@mui/material";
 import React, { type ReactElement } from "react";
 
 interface AboutDialogProps {
@@ -6,32 +6,36 @@ interface AboutDialogProps {
 	setOpen: (value: boolean) => void;
 }
 
+const darkTheme = createTheme({ palette : { mode: 'dark' } });
+
 const AboutDialog = ({ open, setOpen }: AboutDialogProps): ReactElement => (
 	<React.Fragment>
-		<Dialog open={open} onClose={() => setOpen(false)} aria-labelledby="about-dialog-title"
-			aria-describedby="about-dialog-description">
-			<DialogTitle>Conway's Game of Life</DialogTitle>
-			<DialogContent id="alert-dialog-description">
-				Welcome to the Game of Life.
-				There are four rules:
-				<ol>
-					<li>If a cell has fewer than two neighbors it dies of loneliness.</li>
-					<li>If a cell has two or three neighbors it lives to see another day.</li>
-					<li>If a cell has more than three neighbors it dies of overcrowding.</li>
-					<li>If an empty cell has three neighbors, it comes to life!</li>
-				</ol>
-				<p>Create a community by clicking on the grid to bring cells to life (or death).
-					Click <strong>Next</strong> to see what happens to the next generation.
-					Click <strong>Run</strong> to see the simulation run.
-				</p>
-				<p>See <a href="https://en.wikipedia.org/wiki/Conway's_Game_of_Life">
-					Conway's Game of Life</a> on Wikipedia for more information.
-				</p>
-			</DialogContent>
-			<DialogActions>
-				<Button onClick={() => setOpen(false)}>Play!</Button>
-			</DialogActions>
-		</Dialog>
+		<ThemeProvider theme={darkTheme}>
+			<Dialog open={open} onClose={() => setOpen(false)} aria-labelledby="about-dialog-title"
+				aria-describedby="about-dialog-description">
+				<DialogTitle>Conway's Game of Life</DialogTitle>
+				<DialogContent id="alert-dialog-description">
+					Welcome to the Game of Life.
+					There are four rules:
+					<ol>
+						<li>If a cell has fewer than two neighbors it dies of loneliness.</li>
+						<li>If a cell has two or three neighbors it lives to see another day.</li>
+						<li>If a cell has more than three neighbors it dies of overcrowding.</li>
+						<li>If an empty cell has three neighbors, it comes to life!</li>
+					</ol>
+					<p>Create a community by clicking on the grid to bring cells to life (or death).
+						Click <strong>Next</strong> to see what happens to the next generation.
+						Click <strong>Run</strong> to see the simulation run.
+					</p>
+					<p>See <a href="https://en.wikipedia.org/wiki/Conway's_Game_of_Life">
+						Conway's Game of Life</a> on Wikipedia for more information.
+					</p>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={() => setOpen(false)}>Play!</Button>
+				</DialogActions>
+			</Dialog>
+		</ThemeProvider>
 	</React.Fragment>
 );
 
